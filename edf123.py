da84c9a0749dc80233dc9c3f111557cbb5880506
Swastik Khan <121816379+SwastikKhan@users.noreply.github.com>
2024-11-23 11:01:54 +0530

    Update edf123.py

--- edf123.py
+++ edf123.py
@@ -228,5 +228,3 @@ if __name__ == "__main__":
 os.system("ngrok authtoken 2m6ibJzBrgq35SdeD3HUR5Bx7tk_2DgqTd3UFQqayShQCgu4e")
 url = ngrok.connect("http://localhost:8501")
 print(f"Access your Streamlit app here: {url}")
-# Expose the Streamlit app via ngrok
-streamlit run streamlit_app.py
